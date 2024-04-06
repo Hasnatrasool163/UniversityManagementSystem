6f718a11ae5aed7aa9a0b852d96f1f4b2ee325ac
Muhammad Hasnat Rasool <153990457+Hasnatrasool163@users.noreply.github.com>
2024-04-06 05:41:43 +0500

    Added_Source_Code

---
+++ StudentLeaveDetails.java
@@ -0,0 +1,132 @@
+package university.management.system;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.sql.*;
+//import net.proteanit.sql.DbUtils;
+import java.awt.event.*;
+
+public class StudentLeaveDetails extends JFrame implements ActionListener {
+
+    Choice crollno;
+    JTable table;
+    JButton search, print, update, add, cancel;
+    
+    StudentLeaveDetails() {
+        
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Search by Roll Number");
+        heading.setBounds(20, 20, 150, 20);
+        add(heading);
+        
+        crollno = new Choice();
+        crollno.setBounds(180, 20, 150, 20);
+        add(crollno);
+        
+        try {
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("select * from student");
+            while(rs.next()) {
+                crollno.add(rs.getString("rollno"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        table = new JTable();
+        
+        try {
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("select * from studentleave");
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            String[] columnNames = new String[columnCount];
+            for (int i = 1; i <= columnCount; i++) {
+                columnNames[i - 1] = metaData.getColumnName(i);
+            }
+
+            DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+
+            while (rs.next()) {
+                String[] rowData = new String[columnCount];
+                for (int i = 1; i <= columnCount; i++) {
+                    rowData[i - 1] = rs.getString(i);
+                }
+                model.addRow(rowData);
+            }
+
+            table.setModel(model);
+//            table.setModel(DbUtils.resultSetToTableModel(rs));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        JScrollPane jsp = new JScrollPane(table);
+        jsp.setBounds(0, 100, 900, 600);
+        add(jsp);
+        
+        search = new JButton("Search");
+        search.setBounds(20, 70, 80, 20);
+        search.addActionListener(this);
+        add(search);
+        
+        print = new JButton("Print");
+        print.setBounds(120, 70, 80, 20);
+        print.addActionListener(this);
+        add(print);
+        
+        cancel = new JButton("Cancel");
+        cancel.setBounds(220, 70, 80, 20);
+        cancel.addActionListener(this);
+        add(cancel);
+        
+        setSize(900, 700);
+        setLocation(300, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == search) {
+            String query = "select * from studentleave where rollno = '"+crollno.getSelectedItem()+"'";
+            try {
+                Conn c = new Conn();
+                ResultSet rs = c.s.executeQuery(query);
+                ResultSetMetaData metaData = rs.getMetaData();
+                int columnCount = metaData.getColumnCount();
+                String[] columnNames = new String[columnCount];
+                for (int i = 1; i <= columnCount; i++) {
+                    columnNames[i - 1] = metaData.getColumnName(i);
+                }
+
+                DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+
+                while (rs.next()) {
+                    String[] rowData = new String[columnCount];
+                    for (int i = 1; i <= columnCount; i++) {
+                        rowData[i - 1] = rs.getString(i); // Adjust getString for other data types
+                    }
+                    model.addRow(rowData);
+                }
+
+                table.setModel(model);
+//                table.setModel(DbUtils.resultSetToTableModel(rs));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (ae.getSource() == print) {
+            try {
+                table.print();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            setVisible(false);
+        }
+    }
+
+    public static void main(String[] args) {
+        new StudentLeaveDetails();
+    }
+}
